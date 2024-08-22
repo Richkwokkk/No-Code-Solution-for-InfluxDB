30ad61a0a311a9dec3a2d7f7d65155e853a73002
Johnny Jiang <giovannilance94@gmail.com>
2024-08-22 18:54:38 +0930

    fix: fix frontend containerisation

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -11,9 +11,7 @@ export default function Page() {
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
             <h1 className="text-3xl font-bold">Login</h1>
-            <p className="text-balance text-muted-foreground">
-              Enter your email below to login to your account
-            </p>
+            <p className="text-balance text-muted-foreground"></p>
           </div>
           <div className="grid gap-4">
             <div className="grid gap-2">
