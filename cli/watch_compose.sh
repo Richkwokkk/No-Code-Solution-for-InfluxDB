2a6df30b2a9637ab0f5f58fb41abca13b8220941
Johnny Jiang <giovannilance94@gmail.com>
2024-08-24 12:34:15 +0930

    fix: fix docker compose watch for frontend

---
+++ cli/watch_compose.sh
@@ -0,0 +1,9 @@
+docker compose \
+    --file .docker/docker-compose.yaml \
+    --project-directory	./ \
+    --project-name influxdb-pg02 \
+    --env-file .env/influxdb.dev.env \
+    --env-file .env/grafana.dev.env \
+    --env-file .env/server.dev.env \
+    --env-file .env/frontend.dev.env \
+    watch
