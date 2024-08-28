1449c99ea9e817922fa572b963d2c720ede3a9db
Zilin Song <harry.song518@gmail.com>
2024-08-28 13:35:21 +0930

    refactor(frontend): add layout and page for editor

--- frontend/app/editor/layout.tsx
+++ frontend/app/editor/layout.tsx
@@ -1,3 +1,17 @@
-export default function EditorLayout() {
-  return <div>Editor Page</div>;
+import React from "react"
+
+interface EditorLayoutProps {
+  children: React.ReactNode;
 }
+
+const EditorLayout = function ({ children }: EditorLayoutProps) {
+  return (
+    <div className="w-full flex">
+      <div className="hidden h-screen w-screen bg-muted lg:block">
+      {children}
+      </div>
+    </div>
+  );
+};
+
+export default EditorLayout;
