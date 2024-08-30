edd3fa4a79156b01cc9e8cf3859d805cbdfdf8fc
anti-machinee <81377071+anti-machinee@users.noreply.github.com>
2024-08-30 16:42:33 +0930

    feat(backend): Add authentication from app to influxdb

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SignUpView
-
+from .views import (
+    LoginView,
+)
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
+    path("signin/", LoginView.as_view()),
 ]
--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,9 +1,23 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+import requests
+from rest_framework import generics
+from django.http import JsonResponse
 
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+class LoginView(generics.GenericAPIView):
+    def post(self, request):
+        api_url = 'http://0.0.0.0:8086/api/v2/signin'
+        username = request.data.get("username")
+        password = request.data.get("password")
+        try:
+            response = requests.post(api_url, auth=(username, password))
+            # Check if the request was successful
+            if response.status_code == 200:
+                return JsonResponse({"message": "Sign-in successful", "data": response.json()})
+            else:
+                return JsonResponse({
+                    "error": "External API returned an error",
+                    "status_code": response.status_code,
+                    "response": response.text
+                }, status=response.status_code)
+        except requests.exceptions.RequestException as e:
+            return JsonResponse({"error": "Failed to connect to the external API", "details": str(e)}, status=500)
