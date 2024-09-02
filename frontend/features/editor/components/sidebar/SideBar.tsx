f3253d6db708f6af9a44b59ec36c5b1bad4a4645
Zilin Song <harry.song518@gmail.com>
2024-09-02 17:37:23 +0930

    refactor(frontend): optimize sidebar rendering and add toggle functionality

--- frontend/features/editor/components/sidebar/SideBar.tsx
+++ frontend/features/editor/components/sidebar/SideBar.tsx
@@ -1,22 +1,29 @@
 import CustomDraggable from "./CustomDraggable"
 import { nodes } from "../config"
 import { Button } from "@/components/ui/button"
+import { useSidebarToggle } from "@/hooks/use-sidebar-toggle"
+import { cn } from "@/lib/utils"
+import { SidebarToggle } from "./SideBarToggle"
+import { useStore } from "@/hooks/use-store"
 const SideBar = () => {
+  const sidebar = useStore(useSidebarToggle, (state) => state);
+
+  if (!sidebar) return null;
+
   return (
-    <aside className="flex h-full w-60 flex-col border-r border-gray-400">
+    <aside className={cn("fixed flex h-screen w-60 z-20 flex-col border-r translate-x-0 transition-[width] ease-in-out border-gray-400", sidebar?.isOpen === false ? "w-[90px]" : "w-72")}>
+      <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
       <div className="h-full w-full space-y-4 overflow-y-auto p-4 bg-white">
-        {/* <div className="invisible lg:visible absolute z-20">
-          <Button className="inline-flex">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
-          </Button>
-        </div> */}
         {Object.entries(nodes).map(([type, info]) => {
           return (
             <CustomDraggable key={type} id={type} type={type} className="flex w-full cursor-grab items-center space-x-2 rounded-lg border border-gray-400 bg-white p-2">
                 <div className="flex h-10 w-10 items-center justify-center rounded-md bg-gray-100">
                   <info.icon size={20} />
                 </div>
-                <div className="flex flex-col items-start pr-1">
+                <div className={cn("flex flex-col items-start pr-1 ", sidebar?.isOpen === false
+                  ? "hidden"
+                  : ""
+                )}>
                   <p className="text-[10px] opacity-50">{info.label}</p>
                   <p className="text-xs font-bold">{info.title}</p>
                 </div>
